df0b6f2befb57b44ba3ed71d288aeadf86b95f36
mlgmontage <kettleofrassel1@gmail.com>
2020-06-09 16:30:21 +0500

    getting list of todos

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const { ApolloServer, gql } = require("apollo-server");
+const DataStore = require("nedb-promise");
+
+const todos = DataStore({ filename: "data/todos.json", autoload: true });
 
 const typeDefs = gql`
   type Query {
     hello: String!
+    todos: [Todo]
+  }
+
+  type Todo {
+    _id: ID!
+    name: String
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "hello world",
+    todos: async () => await todos.find({}),
   },
 };
 
